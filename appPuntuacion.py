0c1a2a6241fc0328c97481fae4df520e0364635b
marcolopez16 <marcoolopez16@gmail.com>
2024-11-16 09:07:58 +0100

    Update appPuntuacion.py

--- appPuntuacion.py
+++ appPuntuacion.py
@@ -25,15 +25,15 @@ def obtener_datos(ticker_symbol):
         data.get('symbol', 'N/A'),
         round(data.get('trailingPE', 'N/A'), 2) if data.get('trailingPE') else "N/A",
         round(data.get('forwardPE', 'N/A'), 2) if data.get('forwardPE') else "N/A",
-        round(data.get('profitMargins', 'N/A') * 100, 2) if data.get('profitMargins') else "N/A",
+        f"{round(data.get('profitMargins', 'N/A') * 100, 2)}%" if data.get('profitMargins') else "N/A",
         round(data.get('enterpriseToEbitda', 'N/A'), 2) if data.get('enterpriseToEbitda') else "N/A",
-        round(data.get('heldPercentInsiders', 'N/A') * 100, 2) if data.get('heldPercentInsiders') else "N/A",
+        f"{round(data.get('heldPercentInsiders', 'N/A') * 100, 2)}%" if data.get('heldPercentInsiders') else "N/A",
         round(data.get('totalCash', 'N/A'), 2) if data.get('totalCash') else "N/A",
         round(data.get('totalDebt', 'N/A'), 2) if data.get('totalDebt') else "N/A",
         round(data.get('ebitda', 'N/A'), 2) if data.get('ebitda') else "N/A",
-        round(data.get('earningsQuarterlyGrowth', 'N/A') * 100, 2) if data.get('earningsQuarterlyGrowth') else "N/A",
+        f"{round(data.get('earningsQuarterlyGrowth', 'N/A') * 100, 2)}%" if data.get('earningsQuarterlyGrowth') else "N/A",
         round(data.get('beta', 'N/A'), 2) if data.get('beta') else "N/A",
-        round(data.get('dividendYield', 'N/A') * 100, 2) if data.get('dividendYield') else "N/A",
+        f"{round(data.get('dividendYield', 'N/A') * 100, 2)}%" if data.get('dividendYield') else "N/A",
         round(data.get('currentPrice', 'N/A'), 2) if data.get('currentPrice') else "N/A",
         round(data.get('targetMeanPrice', 'N/A'), 2) if data.get('targetMeanPrice') else "N/A",
         ticker.history(period="1d").index[-1].strftime('%Y-%m-%d') if not ticker.history(period="1d").empty else "N/A"
@@ -77,10 +77,10 @@ def main():
         # Nombres de los indicadores correspondientes a cada celda
         indicadores = [
             "Nombre corto", "Símbolo", "P/E trailing", "P/E forward",
-            "Margen de beneficio (%)", "Relación empresa/EBITDA",
-            "Porcentaje de insiders (%)", "Efectivo total",
-            "Deuda total", "EBITDA", "Crecimiento de ganancias trimestrales (%)",
-            "Beta", "Rendimiento del dividendo (%)", "Precio actual",
+            "Margen de beneficio", "Relación empresa/EBITDA",
+            "Porcentaje de insiders", "Efectivo total",
+            "Deuda total", "EBITDA", "Crecimiento de ganancias trimestrales",
+            "Beta", "Rendimiento del dividendo", "Precio actual",
             "Precio objetivo promedio", "Última fecha de actualización"
         ]
 
