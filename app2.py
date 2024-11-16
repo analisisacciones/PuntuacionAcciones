95499088ada3cece4614342651c71b346ce3125e
marcolopez16 <marcoolopez16@gmail.com>
2024-11-16 10:56:29 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,12 +1,12 @@
-import openpyxl
+import xlwings as xw
 import streamlit as st
 
-# Cargar el archivo Excel actualizado
-workbook = openpyxl.load_workbook("Analisis_acciones_actualizado.xlsx")
-sheet = workbook.active
+# Abre el archivo de Excel
+wb = xw.Book('Analisis_acciones_actualizado.xlsx')
+sheet = wb.sheets[0]
 
-# Leer el valor numérico de la celda B2
-valor_b2 = sheet['B2'].value
+# Obtener el valor calculado de la celda AY60
+valor_ay60 = sheet.range('AY60').value
 
 # Mostrar el valor en la interfaz de Streamlit
-st.write(f"El valor de la celda B2 es: {valor_b2}")
+st.write(f"El valor de la celda AY60 es: {valor_ay60}")
