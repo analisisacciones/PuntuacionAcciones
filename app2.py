6fd174cf27c03e79959aa39e6fe0fda41cff3013
marcolopez16 <marcoolopez16@gmail.com>
2024-11-16 10:20:31 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,4 +1,5 @@
 import openpyxl
+import streamlit as st
 
 # Cargar el archivo Excel actualizado
 workbook = openpyxl.load_workbook("Analisis_acciones_actualizado.xlsx")
@@ -8,5 +9,4 @@ sheet = workbook.active
 valor_ay60 = sheet['AY60'].value
 
 # Imprimir el valor
-print("El valor de la celda AY60 es:", valor_ay60)
-
+st.write("El valor de la celda AY60 es:", valor_ay60)
