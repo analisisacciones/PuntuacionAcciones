f93199ef3b5709e541952f90749726e886051b30
marcolopez16 <marcoolopez16@gmail.com>
2024-11-16 10:14:52 +0100

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,12 @@
+import openpyxl
+
+# Cargar el archivo Excel actualizado
+workbook = openpyxl.load_workbook("Analisis_acciones_actualizado.xlsx")
+sheet = workbook.active
+
+# Leer el valor numérico de la celda AY60
+valor_ay60 = sheet['AY60'].value
+
+# Imprimir el valor
+print("El valor de la celda AY60 es:", valor_ay60)
+
