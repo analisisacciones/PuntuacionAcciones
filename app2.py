a7cdc389c8784daf3c64281a9c2b8f4fad72cea3
marcolopez16 <marcoolopez16@gmail.com>
2024-11-16 10:51:36 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -5,14 +5,8 @@ import streamlit as st
 workbook = openpyxl.load_workbook("Analisis_acciones_actualizado.xlsx")
 sheet = workbook.active
 
-# Leer los valores de las celdas B2 a B17
-valores = [sheet[f'B{i}'].value for i in range(2, 18)]
+# Leer el valor numérico de la celda B2
+valor_b2 = sheet['B2'].value
 
-# Mostrar los valores con Streamlit
-st.write("Valores de las celdas B2 a B17:")
-for i, valor in enumerate(valores, start=2):
-    st.write(f"B{i}: {valor}")
-
-# Leer la puntuación de la celda B18
-puntuacion = sheet['B18'].value
-st.write(f"Puntuación de la acción (B18): {puntuacion}")
+# Mostrar el valor en la interfaz de Streamlit
+st.write(f"El valor de la celda B2 es: {valor_b2}")
